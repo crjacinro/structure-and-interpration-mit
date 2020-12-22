98c8e99c56c4645715db5123321703e6765205f4
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-12-23 00:33:58 +0800

    Use Scheme and Chicken compiler

--- introduction/abstraction.scm
+++ introduction/abstraction.scm
@@ -1,22 +1,22 @@
 ; defvar is used instead of DEFINE in CLISP to define a variable
-(defvar A (* 5 5))
+(define A (* 5 5))
 (print (* A A))
 
 ; defun is used instead of DEFINE in CLISP to define a function
 ; to square something, multiply it by itself
-(defun square (x) (* x x))
+(define (square x) (* x x))
 (print (square 10))
 
 ; Average procedure
-(defun average (x y) (/ (+ x y) 2)) 
+(define (average x y) (/ (+ x y) 2)) 
 (print (average 10 20))
 
 ; Absolute Value procedure
-; Using COND
-(defun absvalue(x)
-    (COND ((< x 0) (- x)) 
+; Using condition
+(define (absvalue x)
+    (cond ((< x 0) (- x)) 
             ((= x 0) 0)
-             ((> x 0) X)
+             ((> x 0) x)
     ))
 (print (absvalue 20))
 (print (absvalue -20))
@@ -24,8 +24,8 @@
 
 ; Absolute Value procedure
 ; Using IF
-(defun absvalue2(x)
-    (COND ((< x 0) 
+(define (absvalue2 x)
+    (cond ((< x 0) 
             (- x)
             x) 
     ))
@@ -36,17 +36,17 @@
 
 
 ;Square Root procedures
-(defun squareRoot(x) 
+(define (squareRoot x) 
     (try 1 x))
 
-(defun improve (guess x)
+(define (improve guess x)
     (average guess (/ x guess)))
 
-(defun goodenough? (guess x)
+(define (goodenough? guess x)
     (< (abs (- (square guess) x)) 
         0.1))
         
-(defun try (guess x)
+(define (try guess x)
     (if (goodenough? guess x) guess
     (try (improve guess x) x)))
 
