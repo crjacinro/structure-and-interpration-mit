2562f9632f5dee5f64e9e8071e03461d5ae93367
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-02-23 23:07:19 +0800

    Use code from book for better readability

--- symbolic-differentiation/derivatives.scm
+++ symbolic-differentiation/derivatives.scm
@@ -1,45 +1,40 @@
 (define (deriv exp var)
-    (cond   ((constant? exp var) 0)
-            ((same-var? exp var) 1)
-            ((sum? exp) 
-                (make-sum   (deriv (a1 exp) var)
-                            (deriv (a2 exp) var)))
-            ((product? exp) 
-                (make-sum   
-                    (make-product (m1 exp)
-                                   (deriv (m2 exp) var))
-                    (make-product (deriv (m1 exp) var)
-                                   (m2 exp))))))
+    (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum   (deriv (addend exp) var)
+                                (deriv (augend exp) var)))
+        ((product? exp)
+            (make-sum
+                (make-product   (multiplier exp)
+                                (deriv (multiplicand exp) var))
+                (make-product   (deriv (multiplier exp) var)
+                                (multiplicand exp))))
+        (else
+            (error "unknown expression type: DERIV" exp))))
 
-(define (constant? exp var) 
-    (AND (ATOM? EXP)
-         (NOT (EQ? EXP VAR))))
+(define (variable? x) (symbol? x))
 
-(define (same-var? ex)
-    (AND    (ATOM? EXP)
-            (EQ? EXP VAR)))
+(define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (sum? exp)
-    (and    (not (atom? exp))
-            (eq (car exp) `+)))
+(define (make-sum a1 a2) (list '+ a1 a2))
 
-(define (make-sum a1 a2)
-    list `+ a1 a2)
+(define (make-product m1 m2) (list '* m1 m2))
 
-(define a1 cadr)
-(define a2 caddr)
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
 
-(define (product? exp)
-    (and    (not (atom? exp))
-            (eq (car exp) `*)))
+(define (addend s) (cadr s))
 
-(define (make-product m1 m2)
-    list `* m1 m2)
+(define (augend s) (caddr s))
 
-(define m1 cadr)
-(define m2 caddr)
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
 
-(define foo `x)
+(define (multiplier p) (cadr p))
 
-; not working. prints as procedure.
-(print deriv foo `x)
+(define (multiplicand p) (caddr p))
+
+
+#Test the derivative function
+
+(define test  (deriv '(+ x 3) 'x))
+(print test)
